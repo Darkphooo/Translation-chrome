cab280b6527e8076c4c296fe0aab8f4399ef45a8
darkphooo <dark.xiaoyao@qq.com>
2016-10-19 20:29:21 +0800

    repair audio

--- js/content_script.js
+++ js/content_script.js
@@ -1,13 +1,13 @@
 $(function() {
   $("body").mouseup(function (event) {
-    $('#translation').remove()
+    $("#translation").remove();
     var oDiv = document.createElement('div'); 
     var selection = window.getSelection().toString();
     if(selection != ""){
       oDiv.style.left = event.clientX+'px';
       oDiv.style.top = event.clientY+$(document).scrollTop()+'px';
       oDiv.style.border = '1px solid #000';
-      oDiv.style.background = '#ffeb3b'
+      oDiv.style.background = '#ffff99'
       oDiv.style.position = 'absolute';
       oDiv.style.width = '200px';
       oDiv.style.height = 'auto';
@@ -16,11 +16,17 @@ $(function() {
       document.body.appendChild(oDiv);
       $.get("https://api.shanbay.com/bdc/search/?word="+selection, function (response, status) {
         if(status == "success"){
-          console.log(response);
           if(response.status_code == 0){
-            document.getElementById("translation").innerHTML = "<div><p>"+response.data.content+
+            document.getElementById("translation").innerHTML = "<p>"+response.data.content+
               "</p><p>中文释义："+response.data.definition+
-              "</p><p>音标:"+response.data.pronunciation
+              "</p><p>音标:"+response.data.pronunciation+"</p>"+
+              "<audio id='audio'></audio>"+
+              "<button id='play'>播放</button>";
+            $("#audio").attr("src",response.data.audio_addresses.us[0]);
+            $("#play").mouseup(function (ev) {
+              $("#audio")[0].play();
+              ev.stopPropagation();
+            });
           }else if(response.status_code == 1){
             document.getElementById("translation").innerHTML="<div><p>"+response.msg+"</p></div>"
           }
