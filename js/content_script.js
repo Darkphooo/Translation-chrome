b6d404204b7743b03a33b9b5e0f920d917e3c3a4
darkphooo <dark.xiaoyao@qq.com>
2016-10-19 23:00:33 +0800

    add css to window

--- js/content_script.js
+++ js/content_script.js
@@ -12,16 +12,16 @@ $(function() {
       oDiv.style.width = '200px';
       oDiv.style.height = 'auto';
       oDiv.style.fontSize = '12px';
+      oDiv.style.color = '#000';
       $(oDiv).attr("id","translation");
       document.body.appendChild(oDiv);
       $.get("https://api.shanbay.com/bdc/search/?word="+selection, function (response, status) {
         if(status == "success"){
           if(response.status_code == 0){
-            document.getElementById("translation").innerHTML = "<p>"+response.data.content+
-              "</p><p>中文释义："+response.data.definition+
-              "</p><p>音标:"+response.data.pronunciation+"</p>"+
+            document.getElementById("translation").innerHTML = "<p style='margin-bottom: 0;'>"+response.data.content+"["+response.data.pronunciation+"]"+
+              "</p><p style='margin-bottom: 0;'>释义："+response.data.definition+"</p>"+
               "<audio id='audio'></audio>"+
-              "<button id='play'>播放</button>";
+              "<button id='play' style='display: block; border: none; background-color: transparent; color: #000; box-shadow: none; height: 16px; padding: 0; min-height: 16px;'>►朗读</button>";
             $("#audio").attr("src",response.data.audio_addresses.us[0]);
             $("#play").mouseup(function (ev) {
               $("#audio")[0].play();
